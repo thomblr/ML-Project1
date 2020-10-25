40900e228c7366bfecf78cdbaf2de51258e577a2
thomblr <thomas.1111@live.be>
2020-10-25 20:50:37 +0100

    Add modifications

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
@@ -7,30 +8,64 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 X, y = datasets.load_diabetes(return_X_y=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 # 3
 
-lm = linear_model.LinearRegression()
-lm.fit(X_train, y_train)
+# Adding a column for the error
+X_train = np.c_[X_train, np.ones(X_train.shape[0])]
+X_test = np.c_[X_test, np.ones(X_test.shape[0])]
 
-y_pred = lm.predict(X_test)
+xtrain = np.array(X_train) # X
 
-print("=== OLS ===")
-print("Coefficients: ", lm.coef_)
-print("Mean Squared Error : %.2f" % mean_squared_error(y_test, y_pred))
-print("Coefficient of determination : %.2f" % r2_score(y_test, y_pred))
+xtrain_T = np.transpose(xtrain) # X^T
+
+w = np.matmul(xtrain_T, xtrain) # X^T . X
+inverted = np.linalg.inv(w) # (X^T . X)^-1
+interm = np.dot(inverted, xtrain_T) # (X^T . X)^-1 . X^T
+w_final = np.dot(interm, y_train) # (X^T . X)^-1 . X^T . y
+print(w_final) # Print the best coefficients
+
+y_final = np.dot(X_test, w_final)
+
+score = r2_score(y_test, y_final)
+print(score)
 
 # 4
 
-lasso = linear_model.Lasso(alpha=0.1)
+lasso = linear_model.Lasso(alpha=0.5)
 lasso.fit(X_train, y_train)
 
 y_lasso_pred = lasso.predict(X_test)
-print(lasso.score(X_test, y_test))
 
 print('=== Lasso Model ===')
 print("Coeff: ", lasso.coef_)
-print("MSE: %.2f" % mean_squared_error(y_test, y_lasso_pred))
-print("R2: %.2f" % r2_score(y_test, y_lasso_pred))
+print("R2: %.4f" % r2_score(y_test, y_lasso_pred))
 
+# 5
+
+r2_array = []
+best_r2 = 0
+best_alpha = 0
+for i in np.arange(0.01, 1, 0.01):
+    lasso_bis = linear_model.Lasso(alpha=i)
+    lasso_bis.fit(X_train, y_train)
+    y_lasso_bis_pred = lasso_bis.predict(X_test)
+    result_r2 = r2_score(y_test, y_lasso_bis_pred)
+
+    if best_r2 < result_r2:
+        best_r2 = result_r2
+        best_alpha = i
+
+    r2_array.append(result_r2)
+
+plt.plot(np.arange(0.01, 1, 0.01), r2_array)
+plt.xlabel("Value of Alpha in Lasse Model")
+plt.ylabel("Value of the R2")
+plt.title("Comparison of Alpha and R2 in a Lasso Model")
+plt.show()
+
+# 6
+
+print("Best R2 : %.4f" % best_r2)
+print("Best Alpha : %.4f" % best_alpha)
